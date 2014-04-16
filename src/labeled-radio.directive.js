feb8f29ccf180457f20166a8bf22df8e18917e91
Jed Mao <jed.hunsakergmail>
2014-04-16 11:55:00 -0500

    initial commit, labeled-radio

---
+++ src/labeled-radio.directive.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+module.exports = [
+  function() {
+    return {
+      require: ['ngModel', 'value'],
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'labeled-radio.html',
+      scope: {
+        ngModel: '=',
+        label: '@',
+        name: '@',
+        value: '@',
+        change: '&ngChange'
+      }
+    };
+  }
+];
